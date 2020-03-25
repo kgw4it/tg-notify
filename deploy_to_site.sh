cd255185df2dbbd6da756854957448d4583fbb2d
kgw4it <30734393+kgw4it@users.noreply.github.com>
2020-03-25 09:52:05 +0100

    Update deploy_to_site.sh

--- deploy_to_site.sh
+++ deploy_to_site.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SITE=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SITE=${1:-"NOTSET"}
 
 if [ ! -d "/omd/sites/$SITE" ]; then
   echo "Site '$SITE' does not exist. Aborting."
@@ -8,7 +10,7 @@ if [ ! -d "/omd/sites/$SITE" ]; then
 fi
 
 
-cp local/bin/tg_admin /omd/sites/$SITE/local/bin/tg_admin
-cp local/bin/tg_callback /omd/sites/$SITE/local/bin/tg_callback
-cp local/bin/tg_runner /omd/sites/$SITE/local/bin/tg_runner
-cp local/share/check_mk/notifications/tg_notification_with_callback.py /omd/sites/$SITE/share/check_mk/notifications/tg_notification_with_callback.py
+cp $DIR/local/bin/tg_admin /omd/sites/$SITE/local/bin/tg_admin
+cp $DIR/local/bin/tg_callback /omd/sites/$SITE/local/bin/tg_callback
+cp $DIR/local/bin/tg_runner /omd/sites/$SITE/local/bin/tg_runner
+cp $DIR/local/share/check_mk/notifications/tg_notification_with_callback.py /omd/sites/$SITE/share/check_mk/notifications/tg_notification_with_callback.py
